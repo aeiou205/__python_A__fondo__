9d4d33e34ca6c6b953f0d9e3589f69ceb41f7e05
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-09-21 23:56:57 -0500

    Add files via upload

---
+++ Inicializar_Clases.py
@@ -0,0 +1,12 @@
+class Coche():
+    def __init__(self,color,marca,modelo):
+        self.color = color
+        self.marca = marca
+        self.modelo = modelo
+    
+#if __name__ == '__main__':
+#coche1 = Coche(color='azul', marca ='Honda', modelo ='R16')
+coche2 = Coche('Rojo', 'Mazda' , 'rx8')
+print(coche2.color)
+
+
